9c7e6572321618874b2098eaa44511a7d4c8fe83
andresovichh <ahenderson.holberton@gmail.com>
2022-02-14 17:36:43 -0300

    test

---
+++ 3-quick_sort.c
@@ -0,0 +1,78 @@
+#include "sort.h"
+int partition(int *array, size_t lb, size_t ub, size_t size)
+{
+
+size_t start, end;
+int tmp, pivot;
+
+/** printf("size = %ld", size);*/
+pivot = array[ub];
+start = ub;
+end = lb;
+
+/** printf("getting here 1\n");*/
+while (start > end)
+{
+/** printf("getting here 2 \n");*/
+while(array[start] >= pivot)
+{
+start++;
+/** printf("start = %ld \n", start);*/
+}
+while (array[end] < pivot)
+{
+/**printf("end = %ld \n", end);*/
+end--;
+/** printf("end = %ld \n", end);*/
+}
+if (start > end)
+{
+tmp = array[end];
+array[end] = array[start];
+array[start] = tmp;
+
+}
+}
+tmp = array[end];
+array[end] = array[lb];
+array[lb] = tmp;
+print_array(array, size);
+
+/** printf(" pivot: %ld \n", end);*/
+return end;
+}
+
+/**
+ * @brief 
+ * 
+ *
+ */
+
+void quicksort(int *array, size_t lb, size_t ub, size_t size)
+{
+size_t pivot;
+
+if (lb < ub)
+{
+pivot = partition(array, lb, ub, size);
+/** printf("pivot inside recur: %ld", pivot);*/
+quicksort(array, lb, pivot - 1, size);
+quicksort(array, pivot + 1, ub, size);
+}
+}
+
+
+/**
+ * @brief 
+ * 
+ */
+void quick_sort(int *array, size_t size)
+{
+
+if (!array || size < 2)
+return;
+/**printf("%d", partition(array, lb, ub, size));
+printf("out of the pivot print");
+quicksort(array, 0, size -1, size);*/
+quicksort(array, 0, size -1, size);
+}
